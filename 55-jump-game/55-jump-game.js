d3dfe767e77e38fb47e1de1cb07fed41dd5132a8
robroskie <lroblesky@gmail.com>
2022-05-08 13:34:22 -0700

    Time: 89 ms (74.36%), Space: 46.3 MB (67.04%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,22 +3,17 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    // Set the starting element to the last index of the array
     let currentIdx = goalPostIdx = nums.length - 1;    
     currentIdx--;
 
-    // Iterate from end to start of array
     while(0 <= currentIdx){
-        // If the current element can be reached by the preceding element, update the goalpost index to the preceding element
         if((goalPostIdx - currentIdx) <= nums[currentIdx]){
             goalPostIdx = currentIdx;
         }
         currentIdx--;
     }
-    // If able to assign goalpost index to first element, we are done
     if(goalPostIdx == 0){
         return true;
     }
-    // Otherwise the last element cannot be reached
     return false;
 };
