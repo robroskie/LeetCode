69b8b3e6e0c09343beef1cfd52230d8d04ae9213
robroskie <lroblesky@gmail.com>
2022-05-12 20:19:00 -0700

    Time: 71 ms (63.80%), Space: 42.2 MB (42.68%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -8,18 +8,12 @@ var findMin = function(nums) {
     let right = nums.length - 1;
 
     let mid = Math.floor(nums.length / 2);
-    // console.log(mid);
-    // console.log(`left=${nums[left]}, mid=${nums[mid]}, right=${nums[right]}`);
-    // Search right
+
     do
     {
-        // console.log('vvvvvvvvvvvv');
-        // console.log(`min before=${min}`)
         min = Math.min(min, nums[mid]);
-        // console.log(`min after=${min}`)
 
         if(nums[mid] > nums[right]){
-            // console.log('searching right');
             left = mid; 
             let temp = mid;
             mid = Math.ceil((right - left) / 2) + temp;
@@ -29,11 +23,6 @@ var findMin = function(nums) {
             right = mid;
             mid = Math.ceil((right - left) / 2);
         }
-    
-        // console.log('mid: ' + mid);
-        // console.log(`left=${nums[left]}, mid=${nums[mid]}, right=${nums[right]}`);
-    
-        // console.log('^^^^^^^^^^');
         min = Math.min(min, nums[mid]);
     }
     while(right - left > 1)
