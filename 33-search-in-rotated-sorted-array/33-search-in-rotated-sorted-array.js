0036dbed2a1a147166f98221e534d0491c31352f
robroskie <lroblesky@gmail.com>
2022-05-14 17:48:40 -0700

    Time: 60 ms (93.25%), Space: 42.5 MB (18.78%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -9,38 +9,23 @@ var search = function(nums, target) {
     let mid; 
 
     do{
-
-
         mid = Math.floor((right + left) / 2);
         if(nums[mid] == target)
             return mid;
-        console.log(`left=${left} (${nums[left]}) mid=${mid} (${nums[mid]}) right=${right} (${nums[right]})`);
 
-        // Middle equal to or greater than left
         if(nums[mid] >= nums[left]){
-            // Search right
-            if(target < nums[mid] && target >= nums[left]){
+            if(target < nums[mid] && target >= nums[left])
                 right = mid - 1;
-            }
-            // Search left
-            else{
+            else
                 left = mid + 1;
-            }
         }
-        // Middle less than left 
         else{
-            if(target > nums[mid] && target <= nums[right]){
+            if(target > nums[mid] && target <= nums[right])
                 left = mid + 1;
-            }
-            else{
+            else
                 right = mid - 1;
-            }
         }
-
-        console.log(`left=${left} mid=${mid} right=${right}\n`);
     }
     while(left <= right)
-
     return -1;
-
 };
