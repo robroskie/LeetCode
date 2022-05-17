cdcf011e101f87ef10dd0ded08b51dd4f42c33c2
robroskie <lroblesky@gmail.com>
2022-05-16 18:34:06 -0700

    Time: 104 ms (23.11%), Space: 43.3 MB (29.93%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -3,25 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
+ var twoSum = function(numbers, target) {
     let left = 0;
     let right = numbers.length - 1;
     let curr;
 
     while(right > left){
         curr = numbers[left] + numbers[right];
-        // console.log(`curr = ${curr}      target=${target}`);
-        if(curr == target){
+        if(curr == target)
             return [left + 1, right + 1]
-        }
-        if(curr > target){
+        if(curr > target)
             right -= 1;
-        }    
-        else{
+        else
             left += 1;
-        }
-        
     }
-
     return -1;
 };
