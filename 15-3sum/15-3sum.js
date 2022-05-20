26cee0a7c915b3b85c2d9ea60ff345b85f6043db
robroskie <lroblesky@gmail.com>
2022-05-19 18:54:54 -0700

    Time: 268 ms (30.21%), Space: 52.1 MB (85.76%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -4,63 +4,33 @@
  */
 var threeSum = function(nums) {
     let toReturn = [];
-    // sort the array to be parsed
+
     nums.sort((a, b) => a - b);
-
     for(let i = 0; i < nums.length; i++){
-
-        // prevent duplicates from being found and added to return list
         if(i > 0){
-            while(nums[i] == nums[i - 1] && i < nums.length){
+            while(nums[i] == nums[i - 1] && i < nums.length)
                 i++;
-            }
         }
-
-        // initalize left and right pointers
         let l = i + 1;
         let r = nums.length - 1;
-        
-        // while left pointer is left of the right
+
         while(l < r){
-            // calculate the sum
             let sum = nums[i] + nums[l] + nums[r];
 
-            // if sum equal to 0
-            // push the corresponding three values
-
             if(sum == 0){
-                console.log('pushing above value');
-    
                 toReturn.push([nums[i], nums[l], nums[r]]);
-
-                // move the left pointer until a new value is encountered(since we only want to use each combination once)
                 let previous = l;
                 l++;
-                while(nums[l] == nums[previous]){
+                while(nums[l] == nums[previous])
                     l++;
-                }
-                
-
-
-
-                // reset right pointer to the end of the array so that new combinations can be considered with the updated left pointer
                 r = nums.length - 1;
-
-                
-   
-
             }
-            // if sum is greater than the target value, move the right pointer 1 to the left
-            else if(sum > 0){
+            else if(sum > 0)
                 r -= 1;
-            }
-            // by elimination the sum has to be less than the target value, move the left pointer 1 to the right
-            else{
+            else
                 l += 1;
-            }
             if(l >= nums.length)
                 break;
-
         }
     }
     return toReturn;
