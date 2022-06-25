1dda68c173379dbb1394bcfc6458c490de00e30b
robroskie <lroblesky@gmail.com>
2022-06-25 14:46:12 -0700

    Time: 80 ms (68.46%), Space: 46 MB (12.03%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -4,31 +4,19 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-
     let sum = 0;
     let windowStart = 0;
     let min = nums.length + 1;
 
-    // Iterate over the entire array(sliding window starts at index 0)
     for(let windowEnd = 0; windowEnd < nums.length; windowEnd++){
-        console.log(`adding ${nums[windowEnd]}`)
         sum += nums[windowEnd];
 
-        // While removing the first window element does not cause the sum to drop below the target, keep removing the first window element
         while(sum >= target){
             min = Math.min(windowEnd - windowStart + 1, min);
-            console.log(`removing ${nums[windowStart]}`)
             sum -= nums[windowStart];
             windowStart++;
-
-            console.log(`min=${min}`);
         }
-
-  
     }
-
-
-
     if(min == nums.length + 1)
         return 0;
     return min;
