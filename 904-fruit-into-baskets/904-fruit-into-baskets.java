2cbab8210d6baf164b9e4637fed1fe491998d2e5
robroskie <lroblesky@gmail.com>
2022-06-28 18:48:23 -0700

    Time: 82 ms (45.44%), Space: 105.2 MB (47.78%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -1,27 +1,21 @@
 class Solution {
     public static int totalFruit(int[] fruits) {
-     
     	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     	int startWindow = 0;
     	int max = 0;
     	for(int endWindow = 0; endWindow < fruits.length; endWindow++) {
     		int key = fruits[endWindow];
     		map.merge(key, 1, Integer::sum);
-    		System.out.println(map);
-    		
     		while(map.size() > 2) {
     			int keyToRemove = fruits[startWindow];
     			map.put(keyToRemove, map.get(keyToRemove) - 1);
     			if(map.get(keyToRemove) == 0) {
     				map.remove(keyToRemove);
     			}
-
     			startWindow++;
     		}
     		max = Math.max(max, endWindow - startWindow + 1);
     	}
-    	
-
     	return max;
     }
 }
