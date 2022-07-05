457afbc583906b8d8868609ece524755edf2823d
robroskie <lroblesky@gmail.com>
2022-07-04 20:36:14 -0700

    Time: 2 ms (32.41%), Space: 43.5 MB (60.66%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,42 +14,43 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-//      Queue to hold values popped from tree
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        
-//      Starting value popped from tree to begin algorithm
-        q.offer(root);
-       
-//      Main list which each of the sublists are to be added to
-        List<List<Integer>> mainList = new LinkedList<List<Integer>>();
-        
-//      Edge case
-        if(root == null)
-            return mainList;
-        
-//      Run the algorithm until the queue is empty ==> entire tree has been traversed
-        while(!q.isEmpty()){
-//          size of current level to be iterated over 
-            int len = q.size();
-            
-//          The sublist is used to hold the values for the current level, which are then added to the return list
-            List<Integer> currLvl = new LinkedList<Integer>();
-            for(int i = 0; i < len; i++){
-//              Remove current value from tree and add to list for current lvl
-                TreeNode currVal = q.remove();
-                currLvl.add(currVal.val);
-//              Add the left and right children of the current value to the q, if they exist
-                if(currVal.left != null)
-                    q.add(currVal.left);
-                if(currVal.right != null)
-                    q.add(currVal.right);
-   
-            }
-                mainList.add(currLvl);
-        }
-        
+	public static List<List<Integer>> levelOrder(TreeNode root) {
+		Queue<TreeNode> q = new LinkedList<>();
+		
+		List<List<Integer>> return_list = new LinkedList<List<Integer>>();
+		
+		if(root == null) {
+			return return_list;
+		}
+		
+		q.add(root);
+		
+		while(!q.isEmpty()) {
+			int sizeList = q.size();
+			
+			List<Integer> temp_list = new LinkedList<Integer>();
+			
+			for(int i = 0; i < sizeList; i++) {
+				TreeNode currentNode = q.poll();
+				
+				temp_list.add(currentNode.val);
+				if(currentNode.left != null)
+					q.add(currentNode.left);
+				if(currentNode.right != null)
+					q.add(currentNode.right);
+			}
+			
+			
+	
+			
+			
+			
 
-        return mainList;
-    }
+			
+			
+			return_list.add(temp_list);
+		}
+		
+		return return_list;
+	}
 }
