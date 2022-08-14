dc6612be7db8e718ceed0e3c126d65aa958a8d9a
robroskie <lroblesky@gmail.com>
2022-08-14 12:38:51 -0700

    Time: 110 ms (52.02%), Space: 44.9 MB (63.13%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  let regex = new RegExp('^[a-zA-Z0-9]+$');
+
+  while (left < right) {
+    while (!regex.test(s[left]) && left < right)
+      left++;
+    
+    while (!regex.test(s[right]) && left < right)
+      right--;
+
+    if (s[left].toLowerCase() != s[right].toLowerCase())
+      return false;
+
+    left++;
+    right--;
+  }
+  return true;
+};
